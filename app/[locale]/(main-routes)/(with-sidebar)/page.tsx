ee8e860fdd718b243b67a9dc3bdf49a2438bcab2
Michael Michel <85269483+michael-172@users.noreply.github.com>
2024-12-09 10:26:13 +0200

    remove unused axios interceptor hook from Home component

--- app/[locale]/(main-routes)/(with-sidebar)/page.tsx
+++ app/[locale]/(main-routes)/(with-sidebar)/page.tsx
@@ -2,8 +2,6 @@
 import React from "react";
 import MainHome from "@/app/components/mainHome/MainHome";
 
-import useAxiosInterceptors from "@/app/hooks/useAxiosInterceptors";
-
 export default function Home() {
   // useAxiosInterceptors();
 
