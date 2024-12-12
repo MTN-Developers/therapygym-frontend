fc6a8b936f71dabc6eba8b9d5428643f5aa5bc19
Marina Safwat <66208355+marinaSwadee@users.noreply.github.com>
2024-12-12 15:23:37 +0200

    fix sidebar width in arabic and english and add redux state to it

--- app/components/classroom/VideoPlayer.tsx
+++ app/components/classroom/VideoPlayer.tsx
@@ -1,48 +1,66 @@
 import React from "react";
-import { IVideo } from "@/interfaces";
+import { ICourseVideosResponse, IVideo } from "@/interfaces";
 import dynamic from "next/dynamic";
+import RightSidebar from "@/app/components/classroom/RightSidebar";
 const PlyrVideo = dynamic(() => import("./PlyrVideo"), {
   ssr: false,
 });
 interface IProps {
   src: IVideo;
+  courseVideos: ICourseVideosResponse;
+  handleVideoSelect: (video: IVideo) => void;
+  currentVideo: IVideo | null;
 }
 
-const VideoPlayer = React.memo(({ src }: IProps) => {
-  return (
-    <div
-      style={{
-        position: "relative",
-        height: "540px",
-        backgroundColor: "#2d2f31",
-        overflow: "hidden",
-      }}
-    >
+const VideoPlayer = React.memo(
+  ({
+    src,
+    currentVideo,
+    handleVideoSelect,
+    courseVideos,
+  }: IProps) => {
+    return (
       <div
-        id="player"
         style={{
-          position: "absolute",
-          top: "0",
-          left: "50%",
-          transform: "translateX(-50%)",
-          width: "80%",
-          height: "535px",
-          backgroundColor: "#424242",
+          position: "relative",
+          height: "540px",
+          backgroundColor: "#2d2f31",
+          overflow: "hidden",
         }}
       >
-        {src ? (
-          <PlyrVideo src={src.video_url} />
-        ) : (
-          <>
-            <div className="text-white font-bold w-full h-full flex items-center justify-center">
-              <p> There is no Videos yet .</p>
-            </div>
-          </>
-        )}
+        <div
+          id="player"
+          style={{
+            position: "absolute",
+            top: "0",
+            left: "50%",
+            transform: "translateX(-50%)",
+            width: "80%",
+            height: "535px",
+            backgroundColor: "#424242",
+          }}
+        >
+          <div className="h-full">
+            {src ? (
+              <PlyrVideo src={src.video_url} />
+            ) : (
+              <>
+                <div className="text-white font-bold w-full h-full flex items-center justify-center">
+                  <p> There is no Videos yet .</p>
+                </div>
+              </>
+            )}
+          </div>
+          <RightSidebar
+            chapters={courseVideos}
+            handleVideoSelect={handleVideoSelect}
+            currentVideo={currentVideo}
+          />
+        </div>
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 VideoPlayer.displayName = "VideoPlayer";
 
