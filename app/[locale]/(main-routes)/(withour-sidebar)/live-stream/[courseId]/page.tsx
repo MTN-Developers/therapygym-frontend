ed1a346bb045d0e42322f57d424b19109c2d67c7
Michael Michel <85269483+michael-172@users.noreply.github.com>
2024-12-18 23:40:27 +0200

    feat: integrate Zoom SDK for live streaming functionality and add access token management

--- app/[locale]/(main-routes)/(withour-sidebar)/live-stream/[courseId]/page.tsx
+++ app/[locale]/(main-routes)/(withour-sidebar)/live-stream/[courseId]/page.tsx
@@ -1,5 +1,12 @@
+"use client";
+import ZoomMeeting from "@/app/components/live/ZoomMeeting";
+import { useAppSelector } from "@/app/store/store";
+import { getAll } from "@/services/server";
+import { LoadingOutlined } from "@ant-design/icons";
+import { Spin } from "antd";
 import { useTranslations } from "next-intl";
 import React from "react";
+import useSWR from "swr";
 
 const Page = ({
   params,
@@ -8,11 +15,36 @@ const Page = ({
     courseId: string;
   };
 }) => {
+  const [activeMeeting, setActiveMeeting] = React.useState<any>(null);
   const t = useTranslations("LiveStreamPage");
+  const { isLoading } = useSWR(
+    `/zoom/meetings/course/${params.courseId}`,
+    getAll as any,
+    {
+      onSuccess: (data) => {
+        setActiveMeeting(data?.data?.filter((item: any) => item.is_active)[0]);
+      },
+    }
+  );
+  // console.log(/);
+  const { user } = useAppSelector((state) => state.auth);
   return (
-    <div className="flex flex-col gap-2 w-full items-start justify-start">
+    <div className="flex flex-col gap-2 w-full h-full items-center justify-center">
       <h2 className="font-semibold text-xl"> {t("LiveStream")}</h2>
       <p>{params.courseId}</p>
+      {isLoading ? (
+        <div className="flex justify-center items-center">
+          <Spin indicator={<LoadingOutlined />} />
+        </div>
+      ) : activeMeeting ? (
+        <ZoomMeeting
+          client_name={`${user?.name} - ${user?.country}`}
+          meetingId={activeMeeting.meeting_id}
+          passcode={activeMeeting.passcode}
+        />
+      ) : (
+        <p>{t("NoActiveMeeting")}</p>
+      )}
     </div>
   );
 };
