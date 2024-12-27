ef8fbc03612e896ae547362f8f3dd4bb3752aa93
Michael Michel <85269483+michael-172@users.noreply.github.com>
2024-12-27 22:51:23 +0200

    working in new signup feature

--- i18/request.ts
+++ i18/request.ts
@@ -7,6 +7,6 @@ export default getRequestConfig(async ({ locale }) => {
   if (!locales.includes(locale)) notFound();
 
   return {
-    messages: (await import(`./translation/${locale}.json`)).default,
+    messages: (await import(`@/translation/${locale}.json`)).default,
   };
 });
