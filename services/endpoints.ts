632f73f658df963a8f81e7641ddcfb9f771e8ba6
Michael Michel <85269483+michael-172@users.noreply.github.com>
2024-12-31 17:03:45 +0200

    fix: update PromoCodeForm to use course_id instead of clientPhone and adjust API endpoint accordingly

--- services/endpoints.ts
+++ services/endpoints.ts
@@ -1,5 +1,5 @@
 export const endpoints = {
   login: "/auth/login",
-  checkPromoCode: ({ phone, code }: { phone: string; code: string }) =>
-    `/promo-code/check?phoneNumber=${phone}&code=${code}`,
+  checkPromoCode: ({ code, course_id }: { code: string; course_id }) =>
+    `/promo-code/check?code=${code}&courseId=${course_id}`,
 };
